255add79f2d80d268334c5b6b9f9c8330d28269b
PinGunter <salvaromero08@gmail.com>
2021-09-24 18:11:23 +0200

    archivos iniciales p1

--- pruebasOpenGL/cmake-build-debug/cmake_install.cmake
+++ pruebasOpenGL/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/salva/Escritorio/pruebasOpenGL
+# Install script for directory: /home/salva/InfoGrafica/pruebasOpenGL
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/salva/Escritorio/pruebasOpenGL/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/salva/InfoGrafica/pruebasOpenGL/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
