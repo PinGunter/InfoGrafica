6956ef8f53f2ef10e5213236d2a73b4eadabe44b
Salva Romero <salvaromero08@gmail.com>
2022-01-12 12:18:45 +0100

    jeje ahora no se genera una textura por fram

--- Practica_IG/src/textura.cc
+++ Practica_IG/src/textura.cc
@@ -22,12 +22,13 @@ void Textura::activar() {
     glEnable(GL_TEXTURE_2D);
     if (textura_id == -1){
         glGenTextures(1,&textura_id);
+        glBindTexture(GL_TEXTURE_2D, textura_id);
+        //    gluBuild2DMipmaps(GL_TEXTURE_2D,GL_RGB,width,height,GL_RGB,GL_UNSIGNED_BYTE,data);
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
+    } else {
+        glBindTexture(GL_TEXTURE_2D, textura_id);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     }
-    glBindTexture(GL_TEXTURE_2D, textura_id);
-//    gluBuild2DMipmaps(GL_TEXTURE_2D,GL_RGB,width,height,GL_RGB,GL_UNSIGNED_BYTE,data);
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
 }
